4733e38e417b9fb41bf35a936738f8f687fcaf7a
mauricio diaz <mauro.270@gmail.com>
2024-08-15 16:34:09 -0400

    comit

--- tests/GoogleSerge.spec.js
+++ tests/GoogleSerge.spec.js
@@ -2,7 +2,11 @@ const { test, expect } = require('@playwright/test');
 
 
 test('Google Serge Test', async ({ page }) => {
-    await page.goto('https://www.google.cl/');
+  await page.goto('https://www.google.cl/');
+  await page.getByLabel('Buscar', { exact: true }).fill('pedro');
+  
+    
+    
   
     // Expect a title "to contain" a substring.
     //await expect(page).toHaveTitle(/Playwright/);
