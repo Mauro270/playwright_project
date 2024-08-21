e9541735f5c11ed4f535d6e4acf057b06d034ea7
mauricio diaz <mauro.270@gmail.com>
2024-08-21 19:46:27 -0400

    Nuevos cambios en proyecto

---
+++ tests/TC_CMR_COUNT_001.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test( 'Validar texto (Abre tu cuenta c)', async ({ page }) => {
+  // Recording...
+
+  await page.goto('https://www.bancofalabella.cl/');
+  await page.getByRole('link', { name: 'Cuentas', exact: true }).click();
+  await page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })
+  await expect(page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })).toContainText('Abre tu Cuenta Corriente en 5')
+  await page.close();
+});
