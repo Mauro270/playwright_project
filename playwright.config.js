605e2b34a188933605c2f2bc22628695d4b546d3
mauricio diaz <mauro.270@gmail.com>
2024-08-21 20:32:32 -0400

    Nuevos cambios en brach

--- playwright.config.js
+++ playwright.config.js
@@ -44,7 +44,7 @@ module.exports = defineConfig({
         name: 'chromium',
         use: { 
           ...devices['Desktop Chrome'] ,
-          //viewport: null,
+          viewport:{width:1540,height:738}
       }
     },
 
--- tests/TC_CMR_COUNT_001.spec.ts
+++ tests/TC_CMR_COUNT_001.spec.ts
@@ -4,6 +4,9 @@ test( 'Validar texto (Abre tu cuenta c)', async ({ page }) => {
   // Recording...
 
   await page.goto('https://www.bancofalabella.cl/');
+ console.log(await page.viewportSize()?.width) 
+ console.log(await page.viewportSize()?.height) 
+ 
   await page.getByRole('link', { name: 'Cuentas', exact: true }).click();
   await page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })
   await expect(page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })).toContainText('Abre tu Cuenta Corriente en 5')
