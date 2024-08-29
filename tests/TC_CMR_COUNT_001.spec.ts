16e0b0831cbb19cbf5767370a10b4b190e6d9b01
mauricio diaz <mauro.270@gmail.com>
2024-08-29 16:15:09 -0400

    Nuevo cambio en la prueba

--- tests/TC_CMR_COUNT_001.spec.ts
+++ tests/TC_CMR_COUNT_001.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test( 'Validar texto (Abre tu cuenta c)', async ({ page }) => {
-  // Recording...
+
 
   await page.goto('https://www.bancofalabella.cl/');
  console.log(await page.viewportSize()?.width) 
@@ -11,5 +11,15 @@ test( 'Validar texto (Abre tu cuenta c)', async ({ page }) => {
   await page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })
   await expect(page.getByRole('heading', { name: 'Abre tu Cuenta Corriente en 5' })).toContainText('Abre tu Cuenta Corriente en 5')
   await page.screenshot({ path: './test-results/screenshot.png', fullPage: true });
+  await page.close();
+});
+
+
+test( 'Validar texto (Para que sirve una línea de crédito)', async ({ page }) => {
+  // Realiza mouse hover en un elemento
+
+  await page.goto('https://www.bancofalabella.cl/');
+  await page.waitForTimeout(5000)
+  await page.getByText('Cuentas').first().hover();
   await page.close();
 });
