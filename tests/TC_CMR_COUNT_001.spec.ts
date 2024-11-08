841096f5b38cb3441f11aba16d1f19986940821f
mauricio diaz <mauro.270@gmail.com>
2024-11-08 20:02:22 -0300

    cambios en el test

--- tests/TC_CMR_COUNT_001.spec.ts
+++ tests/TC_CMR_COUNT_001.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test( 'Validar texto (Abre tu cuenta c)', async ({ page }) => {
 
 
-  await page.goto('https://www.bancofalabella.cl/');
+await page.goto('https://www.bancofalabella.cl/');
  console.log(await page.viewportSize()?.width) 
  console.log(await page.viewportSize()?.height) 
  
